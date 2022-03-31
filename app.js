0daf2552b9beefe236a390c411913810f1351424
alenavarro90 <ale.4053@gmail.com>
2022-03-30 23:15:25 -0300

    mercadoLiebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000."));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
